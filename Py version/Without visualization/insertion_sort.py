4d800f704797750a6b75c18ad616c715e8495c15
LucasColas <lucas.colas13@gmail.com>
2020-12-14 19:20:58 +0100

    update

--- Py version/Without visualization/insertion_sort.py
+++ Py version/Without visualization/insertion_sort.py
@@ -2,9 +2,15 @@ def InsertionSort(Board):
     for i in range(1, len(Board)):
         mem = Board[i]
         j = i-1
+
         while j >= 0 and Board[j] > mem:
             Board[j+1] = Board[j]
+            j -= 1
 
         Board[j+1] = mem
-        
+
     return Board
+
+
+Board = [3,4,1,9,7,11,10]
+print(InsertionSort(Board))
